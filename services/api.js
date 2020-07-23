a193df445282a9895ee0f07b51a36ab5b5c44cb3
csxmaiaP <cristhian.smaia@gmail.com>
2020-07-23 10:46:52 -0300

    api link to domain

--- services/api.js
+++ services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:3001' })
-// const api = axios.create({ baseURL: 'http://104.248.13.101:3001' })
-// const api = axios.create({ baseURL: 'http://api.valspotstest.ga' })
+// const api = axios.create({ baseURL: 'http://127.0.0.1:3001' })
+const api = axios.create({ baseURL: 'http://api.valspotstest.ga' })
 
 export default api
